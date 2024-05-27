5ec4a8c8ff1c8a7508128d3c1ad9537133eb4296
locnv_GSV_Development.dept <locnv@glory-software.vn>
2024-05-27 15:01:55 +0700

    feat:send message different service

--- src/employee/consumer/create.consumer.ts
+++ src/employee/consumer/create.consumer.ts
@@ -6,23 +6,23 @@ export class CreateConsumer implements OnModuleInit {
     constructor(private readonly consumer: ConsumerService) { }
 
     async onModuleInit() {
-        this.consumer.consume(
-            'create-client',
-            { topic: 'create-employee' },
-            {
-                eachMessage: async ({ topic, partition, message }) => {
-                    // TODO: write event values into database
-                    console.log("event created ", {
-                        source: 'create-consumer',
-                        message: JSON.parse(message.value as any),
-                        partition: partition.toString(),
-                        topic: topic.toString(),
-                    });
-                    // topic: create-consume
-                    // message.value: data of user and article that the producer sent as a Buffer
-                    // We need convert buffer to string with "toString()"
-                },
-            },
-        );
+        // this.consumer.consume(
+        //     'create-client',
+        //     { topic: 'create-employee' },
+        //     {
+        //         eachMessage: async ({ topic, partition, message }) => {
+        //             // TODO: write event values into database
+        //             console.log("event created ", {
+        //                 source: 'create-consumer',
+        //                 message: JSON.parse(message.value as any),
+        //                 partition: partition.toString(),
+        //                 topic: topic.toString(),
+        //             });
+        //             // topic: create-consume
+        //             // message.value: data of user and article that the producer sent as a Buffer
+        //             // We need convert buffer to string with "toString()"
+        //         },
+        //     },
+        // );
     }
 }
